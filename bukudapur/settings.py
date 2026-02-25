a24c8fc138a712dfe24517ee5567827cf93c6ba9
Andriani <info@acisindonesia.com>
2026-02-25 10:28:55 +0700

    Switch to PostgreSQL for Railway

--- bukudapur/settings.py
+++ bukudapur/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'bukudapur.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("postgresql://postgres:kstkPjTySWsMIwNBQtCVrqNWOoqIOVHi@postgres.railway.internal:5432/railway", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
 
 
